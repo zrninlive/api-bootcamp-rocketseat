054979180e3b93528f363488a33a9fa880f561fd
Christian Oliveira <christian.soliveira@outlook.com>
2020-02-24 19:34:52 -0300

    Add pattern service Abstracting controllers and validations to isolate folders

---
+++ src/app/services/CancelAppointmentService.js
@@ -0,0 +1,50 @@
+import { isBefore, subHours } from 'date-fns';
+
+import Queue from '../../lib/Queue';
+import CancellationMail from '../jobs/CancellationMail';
+
+import User from '../models/User';
+import Appointment from '../models/Appointment';
+
+class CancelAppointmentService {
+  async run({ appointment_id, user_id }) {
+    const appointment = await Appointment.findByPk(appointment_id, {
+      include: [
+        {
+          model: User,
+          as: 'provider',
+          attributes: ['name', 'email'],
+        },
+        {
+          model: User,
+          as: 'user',
+          attributes: ['name'],
+        },
+      ],
+    });
+
+    if (appointment.user_id !== user_id) {
+      throw new Error("You don't have permission to cancel this appointment.");
+    }
+
+    /**
+     * Verify if the current hour is at least 2 hours an advance to cancel
+     */
+    const dateWithSub = subHours(appointment.date, 2);
+
+    if (isBefore(dateWithSub, new Date())) {
+      throw new Error('You can only cancel appointments 2 hours in advance');
+    }
+
+    appointment.canceled_at = new Date();
+    await appointment.save();
+
+    await Queue.add(CancellationMail.key, {
+      appointment,
+    });
+
+    return appointment;
+  }
+}
+
+export default new CancelAppointmentService();
--- src/routes.js
+++ src/routes.js
@@ -13,22 +13,32 @@ import FileController from './app/controllers/FileController';
 import NotificationController from './app/controllers/NotificationController';
 import AvailableController from './app/controllers/AvailableController';
 
+import validateUserStore from './app/validators/UserStore';
+import validateUserUpdate from './app/validators/UserUpdate';
+import validateSessionStore from './app/validators/SessionStore';
+import validateAppointmentStore from './app/validators/AppointmentStore';
+
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 const upload = multer(multerConfig);
 
-routes.post('/users', UserController.store);
-routes.post('/sessions', SessionController.store);
+routes.post('/users', validateUserStore, UserController.store);
+routes.post('/sessions', validateSessionStore, SessionController.store);
 
 routes.use(authMiddleware);
 
-routes.put('/users', UserController.update);
+routes.put('/users', validateUserUpdate, UserController.update);
 
 routes.get('/providers', ProviderController.index);
 routes.get('/providers/:providerId/available', AvailableController.index);
 
-routes.post('/appointments', AppointmentController.store);
+routes.post(
+  '/appointments',
+  validateAppointmentStore,
+  AppointmentController.store
+);
+
 routes.get('/appointments', AppointmentController.index);
 routes.delete('/appointments/:id', AppointmentController.delete);
 
